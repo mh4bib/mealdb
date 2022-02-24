5f948d94ee93d083451d94e09e97b86ed84ad19b
mahiuddinhabib <mh.habib137@gmail.com>
2022-02-24 14:11:40 +0600

    html cards added

--- js/mealdb.js
+++ js/mealdb.js
@@ -3,8 +3,7 @@ const loadFood = () => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${seachFiel.value}`;
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data.meals))
-
+    .then(data => displayResult(data.meals))
     // console.log(url);
     seachFiel.value = '';
 }
