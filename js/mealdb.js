7c7c60ca641b0b0f8604e3cf18e2c0b2e4563e17
mahiuddinhabib <mh.habib137@gmail.com>
2022-02-24 13:41:06 +0600

    fetched food items

---
+++ js/mealdb.js
@@ -0,0 +1,10 @@
+const loadFood = () => {
+    const seachFiel = document.getElementById('search-field');
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${seachFiel.value}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data.meals))
+
+    // console.log(url);
+    seachFiel.value = '';
+}
