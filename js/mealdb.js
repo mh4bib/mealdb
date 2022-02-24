041460176e1bf613378c012f0766a0f4b07dc767
mahiuddinhabib <mh.habib137@gmail.com>
2022-02-24 14:55:21 +0600

    card section js srcipted

--- js/mealdb.js
+++ js/mealdb.js
@@ -6,4 +6,24 @@ const loadFood = () => {
     .then(data => displayResult(data.meals))
     // console.log(url);
     seachFiel.value = '';
+    document.getElementById('result-div').innerHTML = '';
+}
+const displayResult = meals => {
+    const resulDiv = document.getElementById('result-div');
+    meals.forEach(meal => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text text-justify">${meal.strMeal} is a ${meal.strArea} ${meal.strCategory}. Ingredients used to prapare this item is ${meal.strIngredient1}, ${meal.strIngredient2}, ${meal.strIngredient3}, ${meal.strIngredient4}, ${meal.strIngredient5}, ${meal.strIngredient6}, ${meal.strIngredient7}, ${meal.strIngredient8}, ${meal.strIngredient9}, ${meal.strIngredient10} & etc. </p>
+                <p class="card-text">Watch more about the food on <a href="${meal.strYoutube}" target="_blank" class="text-decoration-none">youtube</a></p>
+            </div>
+        </div>
+        `
+        resulDiv.appendChild(div);
+        console.log(meal);
+    });
 }
