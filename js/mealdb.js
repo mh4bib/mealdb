69ac16f34f1b394fb1d2feb661887b78423703e8
mahiuddinhabib <mh.habib137@gmail.com>
2022-02-24 18:42:25 +0600

    no;w nothing found worked

--- js/mealdb.js
+++ js/mealdb.js
@@ -10,20 +10,31 @@ const loadFood = () => {
 const displayResult = meals => {
     const resulDiv = document.getElementById('result-div');
     resulDiv.textContent = '';
-    meals.forEach(meal => {
+    if(meals == null){
         const div = document.createElement('div');
-        div.classList.add('col');
+        div.classList.add('w-100')
         div.innerHTML = `
-        <div class="card h-100">
-            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${meal.strMeal}</h5>
-                <p class="card-text text-justify">${meal.strMeal} is a/an ${meal.strArea} ${meal.strCategory}. Ingredients used to prapare this item is ${meal.strIngredient1}, ${meal.strIngredient2}, ${meal.strIngredient3}, ${meal.strIngredient4}, ${meal.strIngredient5}, ${meal.strIngredient6}, ${meal.strIngredient7}, ${meal.strIngredient8}, ${meal.strIngredient9}, ${meal.strIngredient10} & etc. </p>
-                <p class="card-text">Watch more about the food on <a href="${meal.strYoutube}" target="_blank" class="text-decoration-none">youtube</a></p>
+            <div class="p-5 ">
+                <h5 class="text-center text-muted">Nothing Found</h5>
             </div>
-        </div>
         `
         resulDiv.appendChild(div);
-        console.log(meal);
-    });
+    }
+    else{
+        meals.forEach(meal => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card h-100">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text text-justify">${meal.strMeal} is a/an ${meal.strArea} ${meal.strCategory}. Ingredients used to prapare this item is ${meal.strIngredient1}, ${meal.strIngredient2}, ${meal.strIngredient3}, ${meal.strIngredient4}, ${meal.strIngredient5}, ${meal.strIngredient6}, ${meal.strIngredient7}, ${meal.strIngredient8}, ${meal.strIngredient9}, ${meal.strIngredient10} & etc. </p>
+                    <p class="card-text">Watch more about the food on <a href="${meal.strYoutube}" target="_blank" class="text-decoration-none">youtube</a></p>
+                </div>
+            </div>
+            `
+            resulDiv.appendChild(div);
+        });
+    }
 }
